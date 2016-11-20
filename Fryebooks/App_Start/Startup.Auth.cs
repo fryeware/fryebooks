474ce2f07f52c110cbfcf796ca8c6b425d58def5
david <david@THE-HIPPO>
2016-11-20 12:02:26 -0800

    added support for Google auth.  Styled auth buttons.  removed password.

--- Fryebooks/App_Start/Startup.Auth.cs
+++ Fryebooks/App_Start/Startup.Auth.cs
@@ -74,11 +74,11 @@ namespace Fryebooks
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "26025280534-2fj8l2lvbnkmacvl8k4neqco9bnfio07.apps.googleusercontent.com",
+                ClientSecret = "sB7CTDZA52fZs0mheni5bJOA"
+            });
         }
     }
 }
